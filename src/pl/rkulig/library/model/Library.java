f5fdfa5e6ea83c59878345ef3357c82523861b96
Radek Kulig <radek.kulig@op.pl>
2020-08-04 14:59:58 +0200

    Add Arrays util, add addPublication and remove methods

--- src/pl/rkulig/library/model/Library.java
+++ src/pl/rkulig/library/model/Library.java
@@ -1,12 +1,13 @@
 package pl.rkulig.library.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class Library implements Serializable {
 
-    private static final int MAX_PUBLICATONS = 2000;
+    private static final int INITIAL_CAPACITY = 1;
     private int publicationsNumber;
-    private Publication[] publications = new Publication[MAX_PUBLICATONS];
+    private Publication[] publications = new Publication[INITIAL_CAPACITY];
 
     public Publication[] getPublications() {
         Publication[] result = new Publication[publicationsNumber];
@@ -16,19 +17,31 @@ public class Library implements Serializable {
         return result;
     }
 
-    public void addBook(Book book) {
-        addPublication(book);
-    }
-
-    public void addMagazine(Magazine magazine) {
-        addPublication(magazine);
-    }
-
     public void addPublication(Publication publication) {
-        if (publicationsNumber >= MAX_PUBLICATONS) {
-            throw new ArrayIndexOutOfBoundsException("Max publications exceeded " + MAX_PUBLICATONS);
+        if(publicationsNumber == publications.length) {
+            publications = Arrays.copyOf(publications, publications.length * 2);
         }
         publications[publicationsNumber] = publication;
         publicationsNumber++;
+    }
+
+    public boolean removePublication(Publication pub) {
+        final int NOT_FOUND = -1;
+        int found = NOT_FOUND;
+        int i = 0;
+        while (i < publications.length && found == NOT_FOUND) {
+            if (pub.equals(publications[i])) {
+                found = i;
+            } else {
+                i++;
+            }
+        }
+
+        if (found != NOT_FOUND) {
+            System.arraycopy(publications, found + 1, publications, found, publications.length - found - 1);
+            publicationsNumber--;
+        }
+
+        return found != NOT_FOUND;
     }
 }
